4c21b8932a6bc8d676a1d24436614997a7410b1e
Muhammad Jawwad Khan <140228223+jawwadkhan777@users.noreply.github.com>
2023-09-17 13:09:30 +0500

    Code in Java

---
+++ NumberGuessingGame.java
@@ -0,0 +1,49 @@
+package Projects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    static int randomNumber;
+    static int userNumber;
+    static int attempt = 1;
+    static void gameInstruction(){
+        System.out.print("Game instructions:\n\tA random number stored between 1 to 100 and your objective is to guess that number,\n\tif your guess is accurate then you win otherwise there is a hint for you to win.\n\tFor quit the game you enter -1. Press enter to continue: ");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        System.out.println("--------------Game Starts Now!!!--------------");
+    }
+    static void randomNUmberGenerator(){
+        Random rn = new Random();
+        randomNumber = rn.nextInt(100);
+    }
+    static void userInputResult(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Attempt#"+ attempt);
+        System.out.print("Guess that number: ");
+        userNumber = sc.nextInt();
+        if(userNumber==-1){
+            System.out.println("Game quit and that random number is " + randomNumber);
+            System.out.println("--------------Thanks!!!--------------");
+            return;
+        }
+        else if (userNumber == randomNumber) {
+            System.out.println("You won!, your guess is accurate.");
+            System.out.println("--------------Thanks!!!--------------");
+            return;
+        }
+        else if (userNumber < randomNumber) {
+            System.out.println("Oops, your guess is less than that number.");
+        }
+        else {
+            System.out.println("Oops, your guess is greater than that number.");
+        }
+        attempt = attempt+1;
+        userInputResult();
+    }
+    public static void main(String[] args) {
+        gameInstruction();
+        randomNUmberGenerator();
+        userInputResult();
+    }
+}
+
